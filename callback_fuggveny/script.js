972101dd492bb1160e7355b85110915e6a888bbd
2020b-Németh Noel <85229364+b0nemnoe@users.noreply.github.com>
2024-01-26 10:32:03 +0100

    Add files via upload

---
+++ callback_fuggveny/script.js
@@ -0,0 +1,24 @@
+let szam1, szam2;
+
+function szamol(muvelet, szam1, szam2)
+{
+    let res = muvelet(szam1, szam2);
+    return res;
+}
+const osszead = (szam1, szam2) => szam1+szam2;
+const kivonas = (szam1, szam2) => szam1-szam2;
+const szorzas = (szam1, szam2) => szam1*szam2;
+const osztas = (szam1, szam2) => (szam2 == 0)? "Hibás adat": szam1/szam2;
+
+
+let result = szamol(osszead, 5, 3);
+console.log('Az összeadás eredménye: 8, a számolt érték: ' + result);
+
+result = szamol(kivonas, 8, 2);
+console.log('A kivonás eredménye: 6, a számolt érték: ' + result);
+
+result =  szamol(szorzas, 4, 6);
+console.log('A szorzás eredménye: 24, a számolt érték: ' + result);
+
+result =  szamol(osztas, 9, 3);
+console.log('Az osztás eredménye: 3, a számolt érték: ' + result);
