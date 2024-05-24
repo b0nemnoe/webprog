aeb5eca1060fce36844553b8e00ed3a993522cce
b0nemnoe <b0nemnoe@vasvari.org>
2024-05-24 09:59:45 +0200

    writingReading

--- node/1/js.js
+++ node/1/js.js
@@ -1,23 +1,39 @@
 const fs = require('fs');
 
-if (!fs.existsSync('./1/szoveg')) {
-    fs.mkdir('./1/szoveg', err => {
+if (!fs.existsSync('./szoveg')) {
+    fs.mkdir('./szoveg', err => {
         if (err) {
             console.log(err);
         }
         console.log('mappa létrehozva');
+        Writing();
     });
 } else {
-    fs.writeFile('./1/szoveg/music.txt', 'II: András magyar király, András megoldja simán. Aranybulla-arany-aranybulla', err => {
+    Writing();
+}
+
+
+function Writing() {
+    fs.writeFile('./szoveg/music.txt', 'II: András magyar király, András megoldja simán. Aranybulla-arany-aranybulla', err => {
         if (err) {
             console.log(err);
         }
+        console.log('file létrehozva');
+        Reading();
     });
 }
 
-fs.readFile('./1/szoveg/music.txt', (err, data) => {
-    if (err) {
-        console.log(err);
+
+function Reading() {
+    if (fs.existsSync('./szoveg/music.txt')) {
+        fs.readFile('./szoveg/music.txt', (err, data) => {
+            if (err) {
+                console.log(err);
+            }
+            console.log(data.toString());
+        });   
     }
-    console.log(data.toString());
-});
+    else{
+        console.log('nem létezik a fájl');
+    }
+}
